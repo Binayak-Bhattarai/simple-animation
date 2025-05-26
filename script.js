3cdfa6db7e3a6262fba21c109e7436de2b3ef9e7
Binayak-Bhattarai <binayakbhattaraiofficial.99@gmail.com>
2025-05-26 22:05:01 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+var tl = gsap.timeline();
+
+tl.from("#navbar h3", {
+    y: -50,
+    opacity: 0,
+    delay: 0.3,
+    stagger: 0.3,
+    duration: 0.8
+})
+
+tl.from("#main h1", {
+    x: -500,
+    opacity: 0,
+    stagger: 0.4,
+    duration: 0.8
+})
+
+tl.from(" img ", {
+    x: 100,
+    rotate: 45,
+    opacity:0,
+    duration:0.5,
+    stagger: 0.5
+});
+
+tl.from("#footer h3", {
+    x: -50,
+    opacity: 0,
+     opacity: 0,
+    stagger: 0.4,
+    duration: 0.8
+})
